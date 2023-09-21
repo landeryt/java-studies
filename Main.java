3010fe6623531697e0a13aca43d788ce388cf031
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-21 19:29:52 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Positive{
+
+    public static int sum(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
