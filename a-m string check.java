8118d7153cd7ec0b7a8383b853821b812250bbe7
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-22 11:32:40 +0300

    Update a-m string check.java

--- a-m string check.java
+++ a-m string check.java
@@ -1,6 +1,8 @@
 class Printer {
 //Inputs a string, checks each digit if it's a letter between a-m, returns number of digits that didn't pass the filter along with the string length
     public static String printerError(String s) {
+// return s.replaceAll("[a-m]", "").length() + "/" + s.length(); 
+// Literally just one line of code and beats my entire system??? 
         int max = s.length();
         int good = 0;
         int i;
