94aeb14522a06f55633ba2bda41d353e24d67570
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-24 00:26:16 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Sequence{
+// Fun fact: You can't print arrays directly. Didn't know that.
+    public static int[] reverse(int n){
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++){
+            a[i] = n - i;
+        }
+        return a;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
