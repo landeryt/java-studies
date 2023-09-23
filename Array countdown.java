ef5732b6d1d516be288f2251325cfaba0cc2fd76
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-23 20:27:14 +0300

    Update and rename Main.java to Array countdown.java

--- Array countdown.java
+++ Array countdown.java
@@ -1,5 +1,6 @@
 class Sequence{
 // Fun fact: You can't print arrays directly. Didn't know that.
+    
     public static int[] reverse(int n){
         int[] a = new int[n];
         for (int i = 0; i < n; i++){
