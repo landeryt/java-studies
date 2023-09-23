63153a4c64618411b29ce6acac44b09ba6022bd7
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-23 18:47:20 +0300

    Update and rename Main.java to Reverse String.java

--- Reverse String.java
+++ Reverse String.java
@@ -2,15 +2,14 @@ class Kata
 {
     public static String reverseWords(final String original)
     {
-        /* Gotta get at least 2 placeholders and instead of calling them "null", call them ""
-
-         */
+        /* Gotta get at least 2 placeholders and instead of calling them "null", call them "" */
         String result = "";
         String notOriginal = "";
         for (int i = original.length() - 1; i >= 0; i--) {
             if (original.charAt(i) != ' ') {
                 notOriginal = notOriginal + original.charAt(i);
             } else {
+                // if there's an empty character, put the space before the already placed characters, and also resetting the placeholder in case there's more
                 result = " " + notOriginal + result;
                 notOriginal = "";
             }
