1ac3009856a4f8ea5a37135941a51b8542f745b9
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-24 15:21:04 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Kata {
+    public static boolean feast(String beast, String dish) {
+        beast = beast.toLowerCase();
+        dish = dish.toLowerCase();
+        boolean ok;
+        if (beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length() - 1) == dish.charAt(dish.length() - 1)) {
+            ok = true;
+        } else ok = false;
+        return ok;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
