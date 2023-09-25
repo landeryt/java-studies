8e5dd4448820d8479cbe61f14cf84feda4cc4b1d
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-25 19:49:13 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Kata {
+    public static boolean solution(String str, String ending) {
+        boolean result = false;
+        if (str.endsWith(ending)) {
+            result = true;
+        }
+        return result;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
