6d8fd53bcdc2e3e649ef6c4823b88329969e19a0
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-25 15:36:06 +0300

    Update Grasshopper sum.java

--- Grasshopper sum.java
+++ Grasshopper sum.java
@@ -1,6 +1,8 @@
 class GrassHopper {
 
     public static int summation(int n) {
+        // Here's a one-liner
+        // return  n*(n+1)/2;
         int sum = 0;
         for (int i = 1; i <= n; i++) {
             //The sum always need to be bigger than 0, dummy
