2ad0d3d2a99753b02430580e35eaf4c09c6cb819
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-25 20:35:15 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+class SquareDigit {
+    private static final int BASE = 10;
+
+    public int squareDigits(int n) {
+        if (n < BASE) {
+            return n * n;
+        }
+        int digit = n % BASE;
+        int squaredDigit = digit * digit;
+        return squaredDigit + (squaredDigit < BASE ? BASE : BASE * BASE) * squareDigits(n / BASE);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
