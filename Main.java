713c242859013d4a3d4a7348c231e311a3850769
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-26 14:49:00 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Kata{
+
+    public static String binaryAddition(int a, int b){
+        int c = a + b;
+        String d = Integer.toBinaryString(c);
+        return d;
+
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
