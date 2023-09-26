c95d24b2474b6efca858fa810263ed4fbb290190
Lander <145010136+landeryt@users.noreply.github.com>
2023-09-26 19:22:31 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+class MinMax {
+    public static int[] minMax(int[] arr) {
+        int min = arr[0]; int max = arr[0];
+        // Not exactly sure how to directly return arrays, so I guess I can do this.
+        int result[] = new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+        result[0] = min;
+        result[1] = max;
+        return result;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
