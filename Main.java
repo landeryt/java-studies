64c2f142ba31e739632828663df301c6e188f663
Lander <145010136+landeryt@users.noreply.github.com>
2023-10-06 21:17:43 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Bio {
+    public String dnaToRna(String dna) {
+        dna = dna.replaceAll("T", "U");
+        return dna;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
