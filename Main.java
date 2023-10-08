d7f3ed5968e164fc2933190ca41e7c25202277d6
Lander <145010136+landeryt@users.noreply.github.com>
2023-10-08 19:09:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,12 @@ class Solution {
         boolean yes = true;
         if (pin.length() == 4 | pin.length() == 6) {
             for (char c : pin.toCharArray()) {
+                // Character.isDigit is used to check if the char is a number
                 if (Character.isDigit(c)) {
-                    System.out.println("a");
+                    System.out.println("a"); // this just does nothing.
                 } else {
                     yes = false;
-                    break;
+                    break; // break out of the loop
                 }
             }
         } else yes = false;
